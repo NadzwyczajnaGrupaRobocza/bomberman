e16de542dbaaa83510137242255ff66919d93711
Abergard <littlinker@gmail.com>
2017-03-26 13:40:19 +0200

    add ranges

--- bomberman/Dummy_UT/dummy_class_test.cpp
+++ bomberman/Dummy_UT/dummy_class_test.cpp
@@ -1,8 +1,8 @@
+#include <iostream>
+#include <iterator>
 #include <gtest/gtest.h>
-//#pragma clang diagnostic push
-//#pragma clang diagnostic ignored "-Weverything"
 #include <fakeit.hpp>
-//#pragma clang diagnostic pop
+#include <range/v3/all.hpp>
 
 #include "dummy_class.hpp"
 
@@ -16,6 +16,7 @@ public:
     {
         When(Method(mock, dummyFunction)).Return(56);
     }
+
     Mock<DummyInterface> mock;
     DummyClass2 dummy{mock.get()};
 };
@@ -24,4 +25,34 @@ TEST_F(DummyClassTest, uno)
 {
     EXPECT_EQ(56, dummy.get(34));
     Verify(Method(mock, dummyFunction).Using(34));
+}
+
+TEST(Rangev3, range)
+{
+    const auto NaturalNumberSet = ranges::view::ints(0);
+    const auto digits = NaturalNumberSet | ranges::view::take(10);
+
+    std::cout << "the digits...\n";
+    ranges::copy(digits, ranges::ostream_iterator<int>(std::cout, " "));
+    std::cout << "\nthats odd.. very odd...\n";
+
+    ranges::copy(NaturalNumberSet | ranges::view::filter([](const auto i) {
+                     return i % 2 == 1;
+                 }) | ranges::view::take(42),
+                 ranges::ostream_iterator<int>(std::cout, " "));
+
+    std::cout << "\nand the prime suspects are...\n";
+
+    const auto prime_number = [](const auto n) {
+        return (n < 3)
+                   ? false
+                   : ranges::all_of(ranges::view::ints(2, n - 1),
+                                    [n](const auto i) { return n % i != 0; });
+    };
+
+    ranges::copy(ranges::view::ints(3) | ranges::view::filter(prime_number) |
+                     ranges::view::take(13),
+                 ranges::ostream_iterator<int>(std::cout, " "));
+
+    std::cout << '\n';
 }
