e5063885df8fdc51aeccab6df0f99e1f3b5c400d
Abergard <littlinker@gmail.com>
2017-04-24 22:41:30 +0200

    fix warnings for gtest

--- cmake/compilators/Clang.cmake
+++ cmake/compilators/Clang.cmake
@@ -5,10 +5,4 @@ message(STATUS "Detected system with clang compiler.")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Werror\
  -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-disabled-macro-expansion\
  -Wno-c++98-c++11-compat-pedantic -Wno-weak-vtables -Wno-padded\
- -Wno-global-constructors\
- -Wno-undef -Wno-missing-noreturn\
- -Wno-language-extension-token -Wno-used-but-marked-unused\
- -Wno-shift-sign-overflow")
-
-target_compile_options(gtest PRIVATE "-Wno-deprecated-declarations")
-target_compile_options(gtest_main PRIVATE "-Wno-deprecated-declarations")
+ -Wno-global-constructors")
