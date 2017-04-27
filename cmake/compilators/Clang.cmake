981856f97b8b48934a9e1361b8bf037f0746932c
Abergard <littlinker@gmail.com>
2017-04-27 18:39:48 +0200

    fix for windows

--- cmake/compilators/Clang.cmake
+++ cmake/compilators/Clang.cmake
@@ -7,5 +7,5 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Werror\
  -Wno-c++98-c++11-compat-pedantic -Wno-weak-vtables -Wno-padded\
  -Wno-global-constructors")
 
-target_compile_options(gtest PRIVATE "-Wno-deprecated-declarations")
-target_compile_options(gtest_main PRIVATE "-Wno-deprecated-declarations")
+target_compile_options(gtest PRIVATE "-Wno-deprecated-declarations -Wno-undef")
+target_compile_options(gtest_main PRIVATE "-Wno-deprecated-declarations -Wno-undef")
