e765f0ce4463d23134accebd0a3a96a836ed1e23
Abergard <littlinker@gmail.com>
2017-04-28 01:28:43 +0200

    fix warnings

--- cmake/compilators/Clang.cmake
+++ cmake/compilators/Clang.cmake
@@ -3,8 +3,8 @@ cmake_minimum_required (VERSION 2.8.11)
 message(STATUS "Detected system with clang compiler.")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Werror\
- -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-disabled-macro-expansion\
- -Wno-c++98-c++11-compat-pedantic -Wno-weak-vtables -Wno-padded -Wno-undef\
+ -Wno-c++98-compat -Wno-c++98-compat-pedantic\
+ -Wno-c++98-c++11-compat-pedantic -Wno-weak-vtables -Wno-padded\
  -Wno-global-constructors")
 
 target_compile_options(gtest PRIVATE "-Wno-deprecated-declarations")
