22e749c1bc96e1daeee83a88acf4d7d5edde7c08
Abergard <littlinker@gmail.com>
2017-04-27 18:43:55 +0200

    fix for windows

--- cmake/compilators/Clang.cmake
+++ cmake/compilators/Clang.cmake
@@ -4,8 +4,8 @@ message(STATUS "Detected system with clang compiler.")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Werror\
  -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-disabled-macro-expansion\
- -Wno-c++98-c++11-compat-pedantic -Wno-weak-vtables -Wno-padded\
+ -Wno-c++98-c++11-compat-pedantic -Wno-weak-vtables -Wno-padded -Wno-undef\
  -Wno-global-constructors")
 
-target_compile_options(gtest PRIVATE "-Wno-deprecated-declarations -Wno-undef")
-target_compile_options(gtest_main PRIVATE "-Wno-deprecated-declarations -Wno-undef")
+target_compile_options(gtest PRIVATE "-Wno-deprecated-declarations")
+target_compile_options(gtest_main PRIVATE "-Wno-deprecated-declarations")
