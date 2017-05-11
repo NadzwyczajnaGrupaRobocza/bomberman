8ee207114ddfe14c06d3dbbd431296e1c22e5301
Abergard <littlinker@gmail.com>
2017-05-11 19:50:55 +0200

    disable -Wextra-semi

--- cmake/compilators/Clang.cmake
+++ cmake/compilators/Clang.cmake
@@ -3,7 +3,7 @@ message(STATUS "Detected system with clang compiler.")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Werror\
  -Wno-c++98-compat -Wno-c++98-compat-pedantic\
  -Wno-c++98-c++11-compat-pedantic -Wno-weak-vtables -Wno-padded\
- -Wno-global-constructors")
+ -Wno-global-constructors -Wno-extra-semi")
 
 target_compile_options(gtest PRIVATE "-Wno-deprecated-declarations")
 target_compile_options(gtest_main PRIVATE "-Wno-deprecated-declarations")
