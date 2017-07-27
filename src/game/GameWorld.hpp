490d34db0eef267fdc6cd1249ca410a306bd1693
Przemo <you@example.com>
2017-07-27 09:40:43 +0200

    Adding interfae

--- src/game/GameWorld.hpp
+++ src/game/GameWorld.hpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include "math/Position.hpp"
 #include <boost/serialization/strong_typedef.hpp>
+#include "Bomb.hpp"
 
 class GameWorld
 {
@@ -13,9 +14,11 @@ public:
 
     using Explosion = std::size_t;
 
-    bool is_bomb_at_pos(BombPosition) const = 0;
-    std::unique_ptr<Bomb> create_bomb(BombPosition, TimeToExplode) const = 0;
-    std::unique_ptr<Explosion> create_explosion(ExplosionPosition) const = 0;
+    virtual bool is_bomb_at_pos(BombPosition) const = 0;
+    virtual std::unique_ptr<Bomb> create_bomb(BombPosition, TimeToExplode) const = 0;
+    virtual std::unique_ptr<Explosion> create_explosion(ExplosionPosition) const = 0;
+
+    virtual ~GameWorld() = default;
 };
 
 class BombermanGameWorld : public GameWorld
--- src/math/Position.hpp
+++ src/math/Position.hpp
@@ -7,4 +7,15 @@ struct Position2
     float x;
     float y;
 };
+
+inline bool operator==(const Position2& a, const Position2& b)
+{
+    return std::tie(a.x, a.y) == std::tie(b.x, b.y);
+}
+
+inline bool operator<(const Position2& a, const Position2& b)
+{
+    return std::tie(a.x, a.y) < std::tie(b.x, b.y);
+}
+
 }
