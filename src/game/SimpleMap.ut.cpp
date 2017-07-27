47b11cf3914f286baf291682d3bc89202bd9c3e4
kn65op <kn65op+github@gmail.com>
2017-07-27 10:58:38 +0200

    Create walls with size 5,5

--- src/game/SimpleMap.ut.cpp
+++ src/game/SimpleMap.ut.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 #include "glm/glm.hpp"
 
@@ -13,7 +14,7 @@ public:
     {
         for (int i = 0; i < wallsCount; ++i)
         {
-            physicsEngine.register_colider({}, {});
+            physicsEngine.register_colider({}, {5, 5});
         }
     }
 
@@ -25,22 +26,25 @@ private:
 
 //////////////////////////////////////////////////////////////////////////////////////////TEST
 
-using namespace ::testing;
 using namespace ::fakeit;
 
-class SimpleMapConstructorExpectations : public Test
+class SimpleMapConstructorExpectations : public ::testing::Test
 {
 public:
     SimpleMapConstructorExpectations()
     {
         When(Method(physicsEngine, register_colider))
-            .AlwaysDo([](const auto&, const auto&) {
+            .AlwaysDo([&](const auto& position, const auto& size) {
                 static unsigned id = 0;
+                wallsPositions.push_back(position);
+                wallsSizes.push_back(size);
                 return physics::PhysicsId{id++};
             });
     }
 
     Mock<physics::PhysicsEngine> physicsEngine;
+    std::vector<physics::PhysicsEngine::Position> wallsPositions;
+    std::vector<physics::PhysicsEngine::Position> wallsSizes;
 };
 
 class SimpleMapTest : public SimpleMapConstructorExpectations
@@ -48,9 +52,11 @@ class SimpleMapTest : public SimpleMapConstructorExpectations
 public:
     SimpleMap map{physicsEngine.get()};
     const int wallsCount = 36;
+    physics::PhysicsEngine::Position wallSize{5, 5};
 };
 
-TEST_F(SimpleMapTest, ShouldCreateWalls)
+TEST_F(SimpleMapTest, DuringConstruction_ShouldCreateWalls)
 {
     Verify(Method(physicsEngine, register_colider)).Exactly(wallsCount);
+    ASSERT_THAT(wallsSizes, ::testing::Each(wallSize));
 }
