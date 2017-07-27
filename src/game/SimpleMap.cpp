f6e9f96d328ca0eb52bc04082c56c76005a800b3
kn65op <kn65op+github@gmail.com>
2017-07-27 08:52:00 +0200

    Simple map UT created

---
+++ src/game/SimpleMap.cpp
@@ -0,0 +1,1 @@
+#include "SimpleMap.hpp"
---
+++ src/game/SimpleMap.ut.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+using namespace ::testing;
+
+class SimpleMapTest : public Test
+{
+};
+
+TEST_F(SimpleMapTest, Dummy)
+{
+    ASSERT_TRUE(true);
+}
