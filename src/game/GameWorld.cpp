32a82b9165f27339cbc1a9a025f555d8b0305a37
Przemo <you@example.com>
2017-08-03 08:27:25 +0200

    Overriding functions in Gameworld imp

--- src/game/GameWorld.cpp
+++ src/game/GameWorld.cpp
@@ -1,4 +1,27 @@
 #include "GameWorld.hpp"
 
 
+bool BombermanGameWorld::is_bomb_at_pos(const BombPosition&) const
+{
+    return true;
+}
+
+std::unique_ptr<Bomb> BombermanGameWorld::create_bomb(const BombPosition&, const TimeToExplode) const
+{
+
+    return nullptr;
+}
+
+std::unique_ptr<Explosion> BombermanGameWorld::create_explosion(const ExplosionPosition&) const
+{
+
+    return nullptr;
+}
+
+void BombermanGameWorld::update(ElapsedTime)
+{
+
+    return;
+}
+
 
--- src/game/GameWorld.hpp
+++ src/game/GameWorld.hpp
@@ -4,28 +4,37 @@
 #include "math/Position.hpp"
 #include <boost/serialization/strong_typedef.hpp>
 #include "Bomb.hpp"
+#include <vector>
+
+BOOST_STRONG_TYPEDEF(math::Position2, BombPosition)
+BOOST_STRONG_TYPEDEF(math::Position2, ExplosionPosition)
+BOOST_STRONG_TYPEDEF(std::size_t, TimeToExplode)
+BOOST_STRONG_TYPEDEF(float, ElapsedTime)
+
+using Explosion = std::size_t;
 
 class GameWorld
 {
 public:
-    BOOST_STRONG_TYPEDEF(math::Position2, BombPosition)
-    BOOST_STRONG_TYPEDEF(math::Position2, ExplosionPosition)
-    BOOST_STRONG_TYPEDEF(std::size_t, TimeToExplode)
-
-    using Explosion = std::size_t;
 
     virtual bool is_bomb_at_pos(const BombPosition&) const = 0;
     virtual std::unique_ptr<Bomb> create_bomb(const BombPosition&, const TimeToExplode) const = 0;
     virtual std::unique_ptr<Explosion> create_explosion(const ExplosionPosition&) const = 0;
 
+    virtual void update(ElapsedTime) = 0;
     virtual ~GameWorld() = default;
 };
 
 class BombermanGameWorld : public GameWorld
 {
 public:
+    virtual bool is_bomb_at_pos(const BombPosition&) const override;
+    virtual std::unique_ptr<Bomb> create_bomb(const BombPosition&, const TimeToExplode) const override;
+    virtual std::unique_ptr<Explosion> create_explosion(const ExplosionPosition&) const override;
+    virtual void update(ElapsedTime) override;
 
-
+private:
+    std::vector<Bomb> bombs;
 };
 
 
