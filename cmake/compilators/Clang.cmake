62ff03dfdae9395d625278dc160f428b37d50ba8
kn65op <kn65op+github@gmail.com>
2017-11-23 09:26:53 +0100

    Fix cmakes

--- cmake/compilators/Clang.cmake
+++ cmake/compilators/Clang.cmake
@@ -12,3 +12,4 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Werror\
 target_compile_options(gtest PRIVATE "-Wno-deprecated-declarations")
 target_compile_options(gmock PRIVATE "-Wno-deprecated-declarations")
 target_compile_options(gtest_main PRIVATE "-Wno-deprecated-declarations")
+target_compile_options(gmock_main PRIVATE "-Wno-deprecated-declarations")
