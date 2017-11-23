487e75afdd944dd73f9f74f1e768d84a9d735951
kn65op <kn65op+github@gmail.com>
2017-11-23 09:14:15 +0100

    Extract WallPositionsGenerator to own header

--- src/game/SimpleMap.hpp
+++ src/game/SimpleMap.hpp
@@ -2,23 +2,10 @@
 #include <vector>
 
 #include "ExplosionRange.hpp"
+#include "WallPositionsGenerator.hpp"
 
 #include "glm/glm.hpp"
 #include "physics/PhysicsEngine.hpp"
-
-class WallPositionsGenerator
-{
-public:
-    using BaseType = int;
-    using PositionInSpace = std::pair<BaseType, BaseType>;
-    using WallSize = std::pair<BaseType, BaseType>;
-    using BoundarySize = int;
-    using Wall = std::pair<PositionInSpace, WallSize>;
-    using Walls = std::vector<Wall>;
-
-    virtual ~WallPositionsGenerator() = default;
-    virtual Walls generateBoundaryWalls(BoundarySize) const = 0;
-};
 
 class SimpleMap
 {
---
+++ src/game/WallPositionsGenerator.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+class WallPositionsGenerator
+{
+public:
+    using BaseType = int;
+    using PositionInSpace = std::pair<BaseType, BaseType>;
+    using WallSize = std::pair<BaseType, BaseType>;
+    using BoundarySize = int;
+    using Wall = std::pair<PositionInSpace, WallSize>;
+    using Walls = std::vector<Wall>;
+
+    virtual ~WallPositionsGenerator() = default;
+    virtual Walls generateBoundaryWalls(BoundarySize) const = 0;
+};
