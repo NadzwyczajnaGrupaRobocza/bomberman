61e702c8aff4e114a5bbacbf89ea5150579d87e2
kn65op <kn65op+github@gmail.com>
2017-11-23 09:41:50 +0100

    create walls

--- src/game/BoundaryWallsPositionsGenerator.cpp
+++ src/game/BoundaryWallsPositionsGenerator.cpp
@@ -1,6 +1,17 @@
 #include "BoundaryWallsPositionsGenerator.hpp"
 
-BoundaryWallsPositionsGenerator::Walls BoundaryWallsPositionsGenerator::generateBoundaryWalls(BoundarySize) const
+BoundaryWallsPositionsGenerator::Walls
+    BoundaryWallsPositionsGenerator::generateBoundaryWalls(BoundarySize boundarySize) const
 {
-    return {};
+    if (boundarySize == 0)
+    {
+        return {};
+    }
+    else
+    {
+        return {{{0, 0}, {0, boundarySize}},
+                {{0, 0}, {boundarySize, 0}},
+                {{boundarySize, 0}, {boundarySize, boundarySize}},
+                {{0, boundarySize}, {boundarySize, boundarySize}}};
+    }
 }
--- src/game/BoundaryWallsPositionsGenerator.ut.cpp
+++ src/game/BoundaryWallsPositionsGenerator.ut.cpp
@@ -14,7 +14,23 @@ public:
     BoundaryWallsPositionsGenerator generator;
 };
 
-TEST_F(BoundaryWallsPositionsGeneratorTest, Dummy)
+TEST_F(BoundaryWallsPositionsGeneratorTest, For0SizeShouldReturnNowWalls)
 {
-    ASSERT_FALSE(false);
+    ASSERT_THAT(generator.generateBoundaryWalls(0), IsEmpty());
+}
+
+TEST_F(BoundaryWallsPositionsGeneratorTest, For1SizeShouldReturnOneWall)
+{
+    const WallPositionsGenerator::Walls expectedWalls{
+        {{0, 0}, {0, 1}}, {{0, 0}, {1, 0}}, {{1, 0}, {1, 1}}, {{0, 1}, {1, 1}}};
+
+    ASSERT_THAT(generator.generateBoundaryWalls(1), UnorderedElementsAreArray(expectedWalls));
+}
+
+TEST_F(BoundaryWallsPositionsGeneratorTest, For5SizeShouldReturnOneWall)
+{
+    const WallPositionsGenerator::Walls expectedWalls{
+        {{0, 0}, {0, 5}}, {{0, 0}, {5, 0}}, {{5, 0}, {5, 5}}, {{0, 5}, {5, 5}}};
+
+    ASSERT_THAT(generator.generateBoundaryWalls(5), UnorderedElementsAreArray(expectedWalls));
 }
