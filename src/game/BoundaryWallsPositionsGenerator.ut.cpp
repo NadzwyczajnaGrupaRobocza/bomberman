9a36a0443bbd59582b6f8f9df928980ba1ed3775
kn65op <kn65op+github@gmail.com>
2017-11-23 09:18:51 +0100

    Create BoundaryWallsPositionsGenerator

---
+++ src/game/BoundaryWallsPositionsGenerator.ut.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "fakeit.hpp"
+
+#include "BoundaryWallsPositionsGenerator.hpp"
+
+using namespace ::fakeit;
+using namespace ::testing;
+
+class BoundaryWallsPositionsGeneratorTest : public Test
+{
+};
+
+TEST_F(BoundaryWallsPositionsGeneratorTest, Dummy)
+{
+    ASSERT_FALSE(false);
+}
