46205c6536ccb6099463732cf6628ffb7adb454c
kn65op <kn65op+github@gmail.com>
2017-11-23 08:39:37 +0100

    Disable warnings for Clang

--- cmake/compilators/Clang.cmake
+++ cmake/compilators/Clang.cmake
@@ -10,4 +10,5 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Werror\
  endif()
 
 target_compile_options(gtest PRIVATE "-Wno-deprecated-declarations")
+target_compile_options(gmock PRIVATE "-Wno-deprecated-declarations")
 target_compile_options(gtest_main PRIVATE "-Wno-deprecated-declarations")
