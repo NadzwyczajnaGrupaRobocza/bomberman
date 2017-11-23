4f5aec25b0deda53b299a0ceedf9ff5bed581c1f
Tomasz Drzewiecki <kn65op+github@gmail.com>
2017-11-23 10:01:29 +0100

    Add hyphen

--- cmake/compilators/Clang.cmake
+++ cmake/compilators/Clang.cmake
@@ -3,7 +3,7 @@ message(STATUS "Detected system with clang compiler.")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Werror\
  -Wno-c++98-compat -Wno-c++98-compat-pedantic\
  -Wno-c++98-c++11-compat-pedantic -Wno-weak-vtables -Wno-padded\
- -Wno-global-constructors -Wno-extra-semi -Wnodisabled-macro-expansion")
+ -Wno-global-constructors -Wno-extra-semi -Wno-disabled-macro-expansion")
 
  if(${CMAKE_CXX_COMPILER_VERSION} GREATER 4)
    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-zero-as-null-pointer-constant")
