799a6fa53d0347a697eae06b8b7f1e6286de0128
Abergard <littlinker@gmail.com>
2017-11-25 00:20:53 +0100

    merge with master part two

--- src/game/BoundaryWallsPositionsGenerator.ut.cpp
+++ src/game/BoundaryWallsPositionsGenerator.ut.cpp
@@ -19,28 +19,28 @@ TEST_F(BoundaryWallsPositionsGeneratorTest, For0SizeShouldReturnNowWalls)
     ASSERT_THAT(generator.generate_boundary_walls(0), IsEmpty());
 }
 
-// TEST_F(BoundaryWallsPositionsGeneratorTest, For1SizeShouldReturnOneWall)
-// {
-//     const WallPositionsGenerator::Walls expected_walls{{{0, 0}, {1, 1}}};
+TEST_F(BoundaryWallsPositionsGeneratorTest, For1SizeShouldReturnOneWall)
+{
+    const WallPositionsGenerator::Walls expected_walls{{{0, 0}, {1, 1}}};
 
-//     ASSERT_THAT(generator.generate_boundary_walls(1),
-//                 UnorderedElementsAreArray(expected_walls));
-// }
+    ASSERT_THAT(generator.generate_boundary_walls(1),
+                UnorderedElementsAreArray(expected_walls));
+}
 
-// TEST_F(BoundaryWallsPositionsGeneratorTest, For5SizeShouldReturnOneWall)
-// {
-//     const WallPositionsGenerator::Walls expected_walls{
-//         {{0, 0}, {1, 5}}, {{0, 0}, {5, 1}}, {{4, 0}, {1, 5}}, {{0, 4}, {5, 1}}};
+TEST_F(BoundaryWallsPositionsGeneratorTest, For5SizeShouldReturnOneWall)
+{
+    const WallPositionsGenerator::Walls expected_walls{
+        {{0, 0}, {1, 5}}, {{0, 0}, {5, 1}}, {{4, 0}, {1, 5}}, {{0, 4}, {5, 1}}};
 
-//     ASSERT_THAT(generator.generate_boundary_walls(5),
-//                 UnorderedElementsAreArray(expected_walls));
-// }
+    ASSERT_THAT(generator.generate_boundary_walls(5),
+                UnorderedElementsAreArray(expected_walls));
+}
 
-// TEST_F(BoundaryWallsPositionsGeneratorTest, For2SizeShouldReturnOneWall)
-// {
-//     const WallPositionsGenerator::Walls expected_walls{
-//         {{0, 0}, {1, 2}}, {{0, 0}, {2, 1}}, {{1, 0}, {1, 2}}, {{0, 1}, {2, 1}}};
+TEST_F(BoundaryWallsPositionsGeneratorTest, For2SizeShouldReturnOneWall)
+{
+    const WallPositionsGenerator::Walls expected_walls{
+        {{0, 0}, {1, 2}}, {{0, 0}, {2, 1}}, {{1, 0}, {1, 2}}, {{0, 1}, {2, 1}}};
 
-//     ASSERT_THAT(generator.generate_boundary_walls(2),
-//                 UnorderedElementsAreArray(expected_walls));
-// }
+    ASSERT_THAT(generator.generate_boundary_walls(2),
+                UnorderedElementsAreArray(expected_walls));
+}
--- src/game/SimpleMap.ut.cpp
+++ src/game/SimpleMap.ut.cpp
@@ -8,87 +8,92 @@
 
 using namespace ::fakeit;
 
-// class SimpleMapConstructorExpectations : public ::testing::Test
-// {
-// public:
-//     SimpleMapConstructorExpectations()
-//     {
-//         When(Method(physics_engine, register_colider))
-//             .AlwaysDo([&](const auto& position, const auto& size) {
-//                 static uint8_t id = 0;
-//                 physics_wall_positions.push_back(position);
-//                 physics_wall_sizes.push_back(size);
-//                 const auto pid = physics::PhysicsId{id++};
-//                 physicsIds.push_back(pid);
-//                 return pid;
-//             });
-//         When(Method(physics_engine, deregister)).AlwaysDo([&](const auto id) {
-//             deregistered_physics_ids.push_back(id);
-//         });
-//         Method(wall_positions_generator, generate_boundary_walls)
-//             .Using(edge_size) = generated_walls;
-//         When(Method(render_engine, acquire))
-//             .AlwaysDo([&](const auto& size, const auto& position) {
-//                 static uint8_t id = 0;
-//                 render_wall_positions.push_back(position);
-//                 render_wall_sizes.push_back(size);
-//                 const auto rid = graphics::RendererId{id++};
-//                 render_ids.push_back(rid);
-//                 return rid;
-//             });
-//         When(Method(render_engine, release)).AlwaysDo([&](const auto id) {
-//             deregistered_render_ids.push_back(id);
-//         });
-//         Method(render_engine, acquire)
-//             .Using(boundary_size, top_left_position) = background_id;
-//         render_ids.push_back(background_id);
-//     }
+class SimpleMapConstructorExpectations : public ::testing::Test
+{
+public:
+    SimpleMapConstructorExpectations()
+    {
+        When(Method(physics_engine, register_colider))
+            .AlwaysDo([&](const auto& position, const auto& size) {
+                static uint8_t id = 0;
+                physics_wall_positions.push_back(position);
+                physics_wall_sizes.push_back(size);
+                const auto pid = physics::PhysicsId{id++};
+                physicsIds.push_back(pid);
+                return pid;
+            });
+        When(Method(physics_engine, deregister)).AlwaysDo([&](const auto id) {
+            deregistered_physics_ids.push_back(id);
+        });
+        Method(wall_positions_generator, generate_boundary_walls)
+            .Using(edge_size) = generated_walls;
+        When(Method(render_engine, acquire))
+            .AlwaysDo([&](const auto& size, const auto& position) {
+                static uint8_t id = 0;
+                render_wall_positions.push_back(position);
+                render_wall_sizes.push_back(size);
+                const auto rid = graphics::RendererId{id++};
+                render_ids.push_back(rid);
+                return rid;
+            });
+        When(Method(render_engine, release)).AlwaysDo([&](const auto id) {
+            deregistered_render_ids.push_back(id);
+        });
+        Method(render_engine, acquire).Using(boundary_size, top_left_position) =
+            background_id;
+        render_ids.push_back(background_id);
+    }
 
-//     const int edge_size{10};
-//     const math::Position2f top_left_position{0, 0};
-//     const math::Position2f boundary_size{static_cast<float>(edge_size),
-//                                          static_cast<float>(edge_size)};
-//     const std::vector<physics::PhysicsEngine::Position> generateed_walls_sizes{
-//         {1, 1}, {1, 2}, {4, 4}, {4, 67}};
-//     const std::vector<physics::PhysicsEngine::Position>
-//         generated_walls_positions{{8, 7}, {2, 1}, {2, 8}, {88, 123}};
-//     const WallPositionsGenerator::Walls generated_walls{{{8, 7}, {1, 1}},
-//                                                         {{2, 1}, {1, 2}},
-//                                                         {{2, 8}, {4, 4}},
-//                                                         {{88, 123}, {4, 67}}};
-//     Mock<physics::PhysicsEngine> physics_engine;
-//     Mock<graphics::RendererPool> render_engine;
-//     Mock<WallPositionsGenerator> wall_positions_generator;
-//     std::vector<physics::PhysicsId> physicsIds;
-//     std::vector<physics::PhysicsId> deregistered_physics_ids;
-//     std::vector<physics::PhysicsEngine::Position> physics_wall_positions;
-//     std::vector<physics::PhysicsEngine::Position> physics_wall_sizes;
-//     std::vector<graphics::RendererId> render_ids;
-//     std::vector<graphics::RendererId> deregistered_render_ids;
-//     std::vector<math::Position2f> render_wall_positions;
-//     std::vector<math::Size2f> render_wall_sizes;
-//     graphics::RendererId background_id{uint8_t(1024)};
-// };
+    const int edge_size{10};
+    const math::Position2f top_left_position{0, 0};
+    const math::Size2f boundary_size{static_cast<float>(edge_size),
+                                     static_cast<float>(edge_size)};
+    const std::vector<physics::PhysicsEngine::Position>
+        generated_physics_walls_sizes{{1, 1}, {1, 2}, {4, 4}, {4, 67}};
 
-// class SimpleMapTest : public SimpleMapConstructorExpectations
-// {
-// public:
-//     SimpleMap map{physics_engine.get(), wall_positions_generator.get(),
-//                   render_engine.get()};
+    const std::vector<physics::PhysicsEngine::Position>
+        generated_graphcis_walls_sizes{{1, 1}, {1, 2}, {4, 4}, {4, 67}};
 
-//     void verifyAllWallsArePlacedCorrectly()
-//     {
-//         ASSERT_THAT(
-//             physics_wall_positions,
-//             ::testing::UnorderedElementsAreArray(generated_walls_positions));
-//         ASSERT_THAT(physics_wall_sizes, ::testing::UnorderedElementsAreArray(
-//                                             generateed_walls_sizes));
-//         ASSERT_THAT(render_wall_positions, ::testing::UnorderedElementsAreArray(
-//                                                generated_walls_positions));
-//         ASSERT_THAT(render_wall_sizes, ::testing::UnorderedElementsAreArray(
-//                                            generateed_walls_sizes));
-//     }
-// };
+    const std::vector<physics::PhysicsEngine::Position>
+        generated_walls_positions{{8, 7}, {2, 1}, {2, 8}, {88, 123}};
+    const WallPositionsGenerator::Walls generated_walls{{{8, 7}, {1, 1}},
+                                                        {{2, 1}, {1, 2}},
+                                                        {{2, 8}, {4, 4}},
+                                                        {{88, 123}, {4, 67}}};
+    Mock<physics::PhysicsEngine> physics_engine;
+    Mock<graphics::RendererPool> render_engine;
+    Mock<WallPositionsGenerator> wall_positions_generator;
+    std::vector<physics::PhysicsId> physicsIds;
+    std::vector<physics::PhysicsId> deregistered_physics_ids;
+    std::vector<physics::PhysicsEngine::Position> physics_wall_positions;
+    std::vector<physics::PhysicsEngine::Position> physics_wall_sizes;
+    std::vector<graphics::RendererId> render_ids;
+    std::vector<graphics::RendererId> deregistered_render_ids;
+    std::vector<math::Position2f> render_wall_positions;
+    std::vector<math::Size2f> render_wall_sizes;
+    graphics::RendererId background_id{uint8_t(1024)};
+};
+
+class SimpleMapTest : public SimpleMapConstructorExpectations
+{
+public:
+    SimpleMap map{physics_engine.get(), wall_positions_generator.get(),
+                  render_engine.get()};
+
+    void verifyAllWallsArePlacedCorrectly()
+    {
+        ASSERT_THAT(
+            physics_wall_positions,
+            ::testing::UnorderedElementsAreArray(generated_walls_positions));
+        ASSERT_THAT(physics_wall_sizes, ::testing::UnorderedElementsAreArray(
+                                            generated_physics_walls_sizes));
+        ASSERT_THAT(render_wall_positions,
+                    ::testing::UnorderedElementsAreArray(
+                        generated_graphics_walls_positions));
+        // ASSERT_THAT(render_wall_sizes, ::testing::UnorderedElementsAreArray(
+        //                                    generated_physics_walls_sizes));
+    }
+};
 
 // TEST_F(SimpleMapTest, DuringConstruction_ShouldCreateWalls)
 // {
