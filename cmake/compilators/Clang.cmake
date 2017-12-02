bb51b0fc970e2700c4311b8733bdaa8f4c989f58
kn65op <kn65op@gmail.com>
2017-12-02 14:17:39 +0100

    Use 1z + add noew warning to supress

--- cmake/compilators/Clang.cmake
+++ cmake/compilators/Clang.cmake
@@ -3,7 +3,7 @@ message(STATUS "Detected system with clang compiler.")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything -Werror\
  -Wno-c++98-compat -Wno-c++98-compat-pedantic\
  -Wno-c++98-c++11-compat-pedantic -Wno-weak-vtables -Wno-padded\
- -Wno-global-constructors -Wno-extra-semi
+ -Wno-global-constructors -Wno-extra-semi -Wno-zero-as-null-pointer-constant\
  -std=c++1z")
 
  if(${CMAKE_CXX_COMPILER_VERSION} GREATER 4)
