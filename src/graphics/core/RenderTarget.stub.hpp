812b0b1d25091a712429b6c82fa4e34c24f01ed9
Abergard <littlinker@gmail.com>
2018-01-23 23:41:41 +0100

    change SfmlRenderTarget to template class and create stub for testing purpose

---
+++ src/graphics/core/RenderTarget.stub.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include "SfmlRectangleShape.hpp"
+
+namespace graphics
+{
+namespace
+{
+const Position2f pos{0, 0};
+const Size2f size{0, 0};
+}
+
+class RenderTargetStub
+{
+public:
+    virtual ~RenderTargetStub()
+    {
+        EXPECT_EQ(shouldInitialize, initialized);
+        EXPECT_EQ(shouldClear, cleared);
+        if (shouldClear)
+        {
+            EXPECT_EQ(expectedColor, clearedColor);
+        }
+        EXPECT_EQ(shouldDraw, drawn);
+        if (shouldDraw)
+        {
+            EXPECT_EQ(expectedShape, drawnShape);
+        }
+    }
+    void initialize()
+    {
+        initialized = true;
+    }
+
+    void clear(const sf::Color& color)
+    {
+        cleared = true;
+        clearedColor = color;
+    }
+
+    void draw(const SfmlRectangleShape& shape)
+    {
+        drawn = true;
+        drawnShape = shape;
+    }
+
+    virtual sf::Vector2u getSize() const = 0;
+    virtual bool activate(bool) = 0;
+
+    void expectClear(const sf::Color& color)
+    {
+        shouldClear = true;
+        expectedColor = color;
+    }
+
+    void expectDraw(const SfmlRectangleShape& shape)
+    {
+        shouldDraw = true;
+        expectedShape = shape;
+    }
+
+    bool initialized{false};
+    bool cleared{false};
+    sf::Color clearedColor{sf::Color::Transparent};
+    bool drawn{false};
+    SfmlRectangleShape drawnShape{size, pos};
+
+    bool shouldInitialize{false};
+    bool shouldClear{false};
+    bool shouldDraw{false};
+
+    sf::Color expectedColor{sf::Color::Transparent};
+    SfmlRectangleShape expectedShape{size, pos};
+};
+}
