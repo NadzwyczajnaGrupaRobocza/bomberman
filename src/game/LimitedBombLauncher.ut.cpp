b9156ffa78be8220c59ceed93b61e8f2e6d00550
kn65op <kn65op+github@gmail.com>
2018-01-25 09:06:27 +0100

    Lunching bombs

--- src/game/LimitedBombLauncher.ut.cpp
+++ src/game/LimitedBombLauncher.ut.cpp
@@ -7,8 +7,15 @@ using namespace ::testing;
 class LimitedBombLauncherTest : public Test
 {
 public:
+    const math::Position2 defaultPosition{0.2f, 0.2f};
+    const bool bombHasBeenSpawned = true;
+    const bool bombCannotBeSpawned = false;
+
+    LimitedBombLauncher launcher;
 };
 
-TEST_F(LimitedBombLauncherTest, D)
+TEST_F(LimitedBombLauncherTest, ShouldLaunchBomb)
 {
+    ASSERT_THAT(launcher.try_spawn_bomb(defaultPosition),
+                Eq(bombHasBeenSpawned));
 }
