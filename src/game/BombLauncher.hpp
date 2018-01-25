b414e9486547b66d6c961903adf1bab00c594cdc
kn65op <kn65op+github@gmail.com>
2018-01-25 08:57:53 +0100

    Add inteface

---
+++ src/game/BombLauncher.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "math/Position.hpp"
+
+class BombLauncher
+{
+public:
+    virtual ~BombLauncher() noexcept = default;
+
+    virtual bool try_spawn_bomb(const math::Position2) = 0;
+    virtual void notifyExploded() = 0;
+}
