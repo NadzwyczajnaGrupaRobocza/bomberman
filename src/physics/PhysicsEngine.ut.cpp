b79fda9b2bc3590e999d6cb1b8f421ca801d06a0
kn65op <kn65op+github@gmail.com>
2018-02-01 09:09:51 +0100

    Revert changes in externals

--- src/physics/PhysicsEngine.ut.cpp
+++ src/physics/PhysicsEngine.ut.cpp
@@ -78,7 +78,7 @@ TEST_F(PhysicsEngineInterfaceTest, ShouldHaveRegisterColiderFunction)
     testFunction(engine);
 }
 
-TEST_F(PhysicsEngineInterfaceTest, ShouldHaveDeregisterColiderFunction)
+TEST_F(PhysicsEngineInterfaceTest, ShouldHaveDeegisterColiderFunction)
 {
     auto testFunction = [](physics::PhysicsEngine& engineTested) {
         engineTested.deregister(physics::PhysicsId{1});
