55dcb2e985306ba8da5cd601dcc89d3bbd2dde4b
kn65op <199537+kn65op@users.noreply.github.com>
2019-01-03 08:42:57 +0100

    Reformat mock

--- src/physics/PhysicsEngine.mock.hpp
+++ src/physics/PhysicsEngine.mock.hpp
@@ -10,11 +10,11 @@ namespace physics
 class MockPhysicsEngine : public PhysicsEngine
 {
 public:
-   MOCK_METHOD2( set_position, void (PhysicsId, const math::Position2f&));
-    MOCK_CONST_METHOD1(get_position, math::Position2f (PhysicsId));
-	    MOCK_METHOD0(solve_colisions, void () );
-    MOCK_METHOD2(register_colider, PhysicsId (const math::Size2f&,
-                                       const math::Position2f&));
-    MOCK_METHOD1(deregister, void (PhysicsId) );
+    MOCK_METHOD2(set_position, void(PhysicsId, const math::Position2f&));
+    MOCK_CONST_METHOD1(get_position, math::Position2f(PhysicsId));
+    MOCK_METHOD0(solve_colisions, void());
+    MOCK_METHOD2(register_colider,
+                 PhysicsId(const math::Size2f&, const math::Position2f&));
+    MOCK_METHOD1(deregister, void(PhysicsId));
 };
-}
+} // namespace physics
