8c1379bddb6c09da445a447f6206f554c4552ed9
kn65op <199537+kn65op@users.noreply.github.com>
2019-03-21 08:45:46 +0100

    Remove invalid option

--- cmake/platforms/Windows.cmake
+++ cmake/platforms/Windows.cmake
@@ -12,8 +12,7 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
 
 if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
   set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "/imsvc ")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-  -Xclang")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 endif()
 
 # It is a workaround for boost uuid issue #78
