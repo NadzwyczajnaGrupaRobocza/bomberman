909b79369560d548c3a1218717811063e60b1c33
kn65op <199537+kn65op@users.noreply.github.com>
2019-04-04 09:35:37 +0200

    Remove temporary changes

--- src/game/SimpleMap.ut.cpp
+++ src/game/SimpleMap.ut.cpp
@@ -4,42 +4,38 @@
 #include <vector>
 
 #include "WallPositionsGenerator.mock.hpp"
-#include "fakeit.hpp"
 #include "graphics/renderer_pool.mock.hpp"
 #include "physics/PhysicsEngine.mock.hpp"
 
 #include "SimpleMap.hpp"
 
-using namespace ::fakeit;
+using namespace ::testing;
 
-class SimpleMapConstructorExpectations : public ::testing::Test
+class SimpleMapConstructorExpectations : public Test
 {
 public:
     SimpleMapConstructorExpectations()
     {
-        EXPECT_CALL(physics_engine_mock,
-                    register_colider(testing::_, testing::_))
-            .WillRepeatedly(
-                testing::Invoke([&](const auto& size, const auto& position) {
-                    static uint8_t id = 0;
-                    physics_wall_positions.push_back(position);
-                    physics_wall_sizes.push_back(size);
-                    const auto pid = physics::PhysicsId{id++};
-                    physicsIds.push_back(pid);
-                    return pid;
-                }));
-        EXPECT_CALL(physics_engine_mock, deregister(testing::_))
-            .WillRepeatedly(testing::Invoke([&](const auto id) {
+        EXPECT_CALL(physics_engine, register_colider(_, _))
+            .WillRepeatedly(Invoke([&](const auto& size, const auto& position) {
+                static uint8_t id = 0;
+                physics_wall_positions.push_back(position);
+                physics_wall_sizes.push_back(size);
+                const auto pid = physics::PhysicsId{id++};
+                physicsIds.push_back(pid);
+                return pid;
+            }));
+        EXPECT_CALL(physics_engine, deregister(_))
+            .WillRepeatedly(Invoke([&](const auto id) {
                 deregistered_physics_ids.push_back(id);
             }));
-        EXPECT_CALL(wall_positions_generator_mock,
+        EXPECT_CALL(wall_positions_generator,
                     generate_boundary_walls(edge_size))
-            .WillOnce(testing::Return(generated_walls));
-        EXPECT_CALL(render_engine_mock,
-                    acquire(testing::_, testing::_, testing::_))
-            .WillRepeatedly(testing::Invoke(
+            .WillOnce(Return(generated_walls));
+        EXPECT_CALL(render_engine, acquire(_, _, _))
+            .WillRepeatedly(Invoke(
                 [&](const auto& size, const auto& position, const auto& color) {
-                    static uint8_t id = 0;
+                    static uint8_t id = 80;
                     render_wall_positions.push_back(position);
                     render_wall_sizes.push_back(size);
                     wall_colors.push_back(color);
@@ -47,13 +43,13 @@ public:
                     render_ids.push_back(rid);
                     return rid;
                 }));
-        EXPECT_CALL(render_engine_mock, release(testing::_))
-            .WillRepeatedly(testing::Invoke(
+        EXPECT_CALL(render_engine, release(_))
+            .WillRepeatedly(Invoke(
                 [&](const auto id) { deregistered_render_ids.push_back(id); }));
         constexpr graphics::color map_grey{161, 161, 161};
-        EXPECT_CALL(render_engine_mock,
+        EXPECT_CALL(render_engine,
                     acquire(boundary_size, top_left_position, map_grey))
-            .WillRepeatedly(testing::Return(background_id));
+            .WillRepeatedly(Return(background_id));
         render_ids.push_back(background_id);
     }
 
@@ -71,10 +67,9 @@ public:
                                                         {{2, 1}, {1, 2}},
                                                         {{2, 8}, {4, 4}},
                                                         {{88, 123}, {4, 67}}};
-    testing::StrictMock<physics::MockPhysicsEngine> physics_engine_mock;
-    testing::StrictMock<graphics::mock_renderer_pool> render_engine_mock;
-    testing::StrictMock<MockWallPositionsGenerator>
-        wall_positions_generator_mock;
+    StrictMock<physics::MockPhysicsEngine> physics_engine;
+    StrictMock<graphics::mock_renderer_pool> render_engine;
+    StrictMock<MockWallPositionsGenerator> wall_positions_generator;
     std::vector<physics::PhysicsId> physicsIds;
     std::vector<physics::PhysicsId> deregistered_physics_ids;
     std::vector<math::Position2f> physics_wall_positions;
@@ -90,23 +85,21 @@ public:
 class SimpleMapTest : public SimpleMapConstructorExpectations
 {
 public:
-    SimpleMap map{physics_engine_mock, wall_positions_generator_mock,
-                  render_engine_mock};
+    SimpleMap map{physics_engine, wall_positions_generator, render_engine};
 
     void verifyAllWallsArePlacedCorrectly()
     {
-        ASSERT_THAT(
-            physics_wall_positions,
-            ::testing::UnorderedElementsAreArray(generated_walls_positions));
-        ASSERT_THAT(physics_wall_sizes, ::testing::UnorderedElementsAreArray(
-                                            generated_walls_sizes));
-        ASSERT_THAT(render_wall_positions, ::testing::UnorderedElementsAreArray(
-                                               generated_walls_positions));
-        ASSERT_THAT(render_wall_sizes, ::testing::UnorderedElementsAreArray(
-                                           generated_walls_sizes));
+        ASSERT_THAT(physics_wall_positions,
+                    UnorderedElementsAreArray(generated_walls_positions));
+        ASSERT_THAT(physics_wall_sizes,
+                    UnorderedElementsAreArray(generated_walls_sizes));
+        ASSERT_THAT(render_wall_positions,
+                    UnorderedElementsAreArray(generated_walls_positions));
+        ASSERT_THAT(render_wall_sizes,
+                    UnorderedElementsAreArray(generated_walls_sizes));
 
         constexpr graphics::color wall_bronze{205, 127, 50};
-        ASSERT_THAT(wall_colors, ::testing::Each(wall_bronze));
+        ASSERT_THAT(wall_colors, Each(wall_bronze));
     }
 };
 
@@ -120,7 +113,7 @@ TEST_F(SimpleMapTest,
 {
     ExplosionRange expected_range{1_left, 1_right, 1_up, 1_down};
     ASSERT_THAT(map.get_explosion_range(std::make_pair(4, 4), 1),
-                ::testing::Eq(expected_range));
+                Eq(expected_range));
 }
 
 TEST_F(SimpleMapTest,
@@ -128,7 +121,7 @@ TEST_F(SimpleMapTest,
 {
     ExplosionRange expected_range{2_left, 2_right, 2_up, 2_down};
     ASSERT_THAT(map.get_explosion_range(std::make_pair(4, 4), 2),
-                ::testing::Eq(expected_range));
+                Eq(expected_range));
 }
 
 TEST_F(SimpleMapTest,
@@ -136,7 +129,7 @@ TEST_F(SimpleMapTest,
 {
     ExplosionRange expected_range{0_left, 1_right, 1_up, 1_down};
     ASSERT_THAT(map.get_explosion_range(std::make_pair(1, 2), 1),
-                ::testing::Eq(expected_range));
+                Eq(expected_range));
 }
 
 TEST_F(SimpleMapTest,
@@ -144,7 +137,7 @@ TEST_F(SimpleMapTest,
 {
     ExplosionRange expected_range{2_left, 3_right, 0_up, 3_down};
     ASSERT_THAT(map.get_explosion_range(std::make_pair(3, 1), 3),
-                ::testing::Eq(expected_range));
+                Eq(expected_range));
 }
 
 TEST_F(SimpleMapTest,
@@ -152,7 +145,7 @@ TEST_F(SimpleMapTest,
 {
     ExplosionRange expected_range{3_left, 3_right, 3_up, 2_down};
     ASSERT_THAT(map.get_explosion_range(std::make_pair(5, 6), 3),
-                ::testing::Eq(expected_range));
+                Eq(expected_range));
 }
 
 TEST_F(SimpleMapTest,
@@ -160,7 +153,7 @@ TEST_F(SimpleMapTest,
 {
     ExplosionRange expected_range{3_left, 2_right, 3_up, 0_down};
     ASSERT_THAT(map.get_explosion_range(std::make_pair(6, 9), 3),
-                ::testing::Eq(expected_range));
+                Eq(expected_range));
 }
 
 class SimpleMapWithoutImplicitConstructionTest
@@ -172,12 +165,10 @@ TEST_F(SimpleMapWithoutImplicitConstructionTest,
        DuringDestruction_ShouldUnredisterWalls)
 {
     {
-        SimpleMap map_destructor_test{physics_engine_mock,
-                                      wall_positions_generator_mock,
-                                      render_engine_mock};
+        SimpleMap map_destructor_test{physics_engine, wall_positions_generator,
+                                      render_engine};
     }
-    ASSERT_THAT(deregistered_render_ids,
-                ::testing::UnorderedElementsAreArray(render_ids));
+    ASSERT_THAT(deregistered_render_ids, UnorderedElementsAreArray(render_ids));
     ASSERT_THAT(deregistered_physics_ids,
-                ::testing::UnorderedElementsAreArray(physicsIds));
+                UnorderedElementsAreArray(physicsIds));
 }
