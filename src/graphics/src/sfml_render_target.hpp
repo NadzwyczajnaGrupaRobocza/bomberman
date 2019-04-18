8140d8b571fff122695d4b25aaa8388826d42ea3
Abergard <littlinker@gmail.com>
2019-04-18 21:12:36 +0200

    Merge use of view from abergard

--- src/graphics/src/sfml_render_target.hpp
+++ src/graphics/src/sfml_render_target.hpp
@@ -3,12 +3,54 @@
 #include <memory>
 
 #include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/View.hpp>
 
 #include "context_renderer.hpp"
 #include "window_size.hpp"
 
 namespace graphics
 {
+namespace
+{
+//https://github.com/SFML/SFML/wiki/Source:-Letterbox-effect-using-a-view
+sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight)
+{
+
+    // Compares the aspect ratio of the window to the aspect ratio of the view,
+    // and sets the view's viewport accordingly in order to archieve a letterbox
+    // effect. A new view (with a new viewport set) is returned.
+
+    float windowRatio = windowWidth / (float)windowHeight;
+    float viewRatio = view.getSize().x / (float)view.getSize().y;
+    float sizeX = 1;
+    float sizeY = 1;
+    float posX = 0;
+    float posY = 0;
+
+    bool horizontalSpacing = true;
+    if (windowRatio < viewRatio)
+        horizontalSpacing = false;
+
+    // If horizontalSpacing is true, the black bars will appear on the left and
+    // right side. Otherwise, the black bars will appear on the top and bottom.
+
+    if (horizontalSpacing)
+    {
+        sizeX = viewRatio / windowRatio;
+        posX = (1 - sizeX) / 2.f;
+    }
+
+    else
+    {
+        sizeY = windowRatio / viewRatio;
+        posY = (1 - sizeY) / 2.f;
+    }
+
+    view.setViewport(sf::FloatRect(posX, posY, sizeX, sizeY));
+
+    return view;
+}
+}
 template <typename BaseRenderTarget>
 class render_target : public context_renderer, public BaseRenderTarget
 {
@@ -32,6 +74,17 @@ public:
         BaseRenderTarget::draw(drawable);
     }
 
+    void set_view() override
+    {
+        float map_width{100};
+        float map_height{100};
+
+        view.setSize(map_width, map_height); // <- it should be set once, at the startup
+        view.setCenter(view.getSize().x / 2, view.getSize().y / 2); // <- only at startup
+        view = getLetterboxView( view, size.width, size.height); // <- at startup and every time the window size is change
+        BaseRenderTarget::setView(view);
+    }
+
     sf::Vector2u getSize() const override
     {
         return size;
@@ -44,6 +97,7 @@ public:
 
 protected:
     window_size size;
+    sf::View view;
 };
 
 using sfml_render_target = render_target<sf::RenderTarget>;
