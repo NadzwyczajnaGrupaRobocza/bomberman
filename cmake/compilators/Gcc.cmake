7b2ff32976a14e3cfdca8fbb26d0f76179072ed9
Abergard <littlinker@gmail.com>
2019-05-09 22:56:33 +0200

    change msvc level warning to 4

--- cmake/compilators/Gcc.cmake
+++ cmake/compilators/Gcc.cmake
@@ -1,4 +1,4 @@
-message(STATUS "Detected system with gcc compiler.")
+message(STATUS "Detected system with gcc compiler v${CMAKE_CXX_COMPILER_VERSION}")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
 -Wall \
--- cmake/compilators/Msvc.cmake
+++ cmake/compilators/Msvc.cmake
@@ -1,7 +1,6 @@
-message(STATUS "Detected system with Msvc compiler.")
+message(STATUS "Detected system with Msvc compiler v${CMAKE_CXX_COMPILER_VERSION}")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc /Wall\
- /wd4251 /wd4265 /wd4350 /wd4371 /wd4514 /wd5031 /wd5026 /wd5027\
- /wd4625 /wd4619 /wd4626 /wd4710 /wd4711 /wd4820 /wd4668")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4 /experimental:external /external:anglebrackets \
+ /wd4201 /wd4309")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /D not=! /D and=&& /D or=||")
