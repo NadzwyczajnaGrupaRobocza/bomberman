d2936f429e0b49f1be17d93cb32021d786d5529c
kn65op <199537+kn65op@users.noreply.github.com>
2019-06-06 08:10:54 +0200

    Revert removal of Gcc cmake

---
+++ cmake/compilators/Gcc.cmake
@@ -0,0 +1,18 @@
+message(STATUS "Detected system with gcc compiler.")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
+-Wall \
+-Wextra \
+-Wconversion \
+-Wfloat-equal \
+-Wunreachable-code \
+-Wswitch-enum \
+-Wmissing-declarations \
+-Wshadow \
+-Wnon-virtual-dtor \
+-pedantic \
+-g ")
+
+if(TREAT_WARNING_AS_ERROR)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
+endif()
