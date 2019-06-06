fb69d610f8f08d2cb62e86987ca79ae56c68d040
kn65op <199537+kn65op@users.noreply.github.com>
2019-06-06 09:45:08 +0200

    Scale function

--- src/game/FieldSize.hpp
+++ src/game/FieldSize.hpp
@@ -9,15 +9,19 @@ constexpr auto field_height = 11;
 constexpr auto field_size = math::Size2i{field_width, field_height};
 constexpr auto field_size_f = math::Size2f{field_width, field_height};
 
-template <typename Type, typename = std::enable_if_t<
-                             std::is_arithmetic<decltype(Type::first)>::value>>
+template <typename>
+struct to_int_type
+{
+    using type = int;
+};
+
+template <typename Type, typename to_int_type<decltype(Type::first)>::type = 0>
 Type scale_to_field_size(Type val)
 {
     return {val.first * field_width, val.second * field_height};
 }
 
-template <typename Type, typename = std::enable_if_t<
-                             std::is_arithmetic<decltype(Type::width)>::value>>
+template <typename Type, typename to_int_type<decltype(Type::width)>::type = 0>
 Type scale_to_field_size(Type val)
 {
     return {val.width * field_width, val.height * field_height};
