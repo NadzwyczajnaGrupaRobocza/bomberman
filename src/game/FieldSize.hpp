dc2e748786769a2bda91f165bf1b41696ac0fb4a
kn65op <199537+kn65op@users.noreply.github.com>
2019-06-27 09:19:08 +0200

    Change fields size to 1,1

--- src/game/FieldSize.hpp
+++ src/game/FieldSize.hpp
@@ -3,8 +3,8 @@
 #include "math/Size2f.hpp"
 #include "math/Size2i.hpp"
 
-constexpr auto field_width = 11;
-constexpr auto field_height = 11;
+constexpr auto field_width = 1;
+constexpr auto field_height = 1;
 
 constexpr auto field_size = math::Size2i{field_width, field_height};
 constexpr auto field_size_f = math::Size2f{field_width, field_height};
