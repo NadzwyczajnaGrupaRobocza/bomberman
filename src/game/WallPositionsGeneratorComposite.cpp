edfe0fa026fb159686ee4fb4c7162612665afd96
Tomasz Drzewiecki <tomasz.drzewiecki@nokia.com>
2019-12-19 09:23:08 +0100

    Add files

---
+++ src/game/WallPositionsGeneratorComposite.cpp
@@ -0,0 +1,1 @@
+#include "WallsPositionGeneratorComposite.hpp"
---
+++ src/game/WallPositionsGeneratorComposite.ut.cpp
@@ -0,0 +1,17 @@
+#include "gmock/gmock.h"
+
+#include "WallsPositionGeneratorComposite.hpp"
+
+using namespace ::testing;
+
+class WallsPositionGeneratorCompositeTest
+{
+public:
+    WallsPositionGeneratorComposite generator
+    {
+    }
+};
+
+TEST_F(WallsPositionGeneratorCompositeTest, shouldCallInsideGenerators)
+{
+}
